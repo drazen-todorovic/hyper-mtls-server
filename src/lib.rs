dd9f43f4e6cf946da75e988718531bbeb005df68
Drazen Todorovic <drazen.todorovic@gmail.com>
2024-03-29 18:56:04 +0100

    Crates upgrade

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,15 @@
 use crate::Error::{
-    CertExtractError, CertFileReadError, PrivateKeyExtractError, PrivateKeyFileReadError,
-    PrivateKeyItemEmptyError, PrivateKeyUnsupportedFormat, ServeListenerError, ServerConfigError,
-    TrustStoreError,
+    CertFileReadError, PrivateKeyExtractError, PrivateKeyFileReadError,
+    PrivateKeyItemEmptyError, ServerConfigError, TrustStoreError,
 };
-use futures_util::future::poll_fn;
-use hyper::server::accept::Accept;
-use hyper::server::conn::{AddrIncoming, AddrStream};
-use rustls::server::AllowAnyAuthenticatedClient;
-use rustls::{Certificate, PrivateKey, RootCertStore, ServerConfig};
-use rustls_pemfile::Item;
+use rustls::server::WebPkiClientVerifier;
+use rustls::{RootCertStore, ServerConfig};
+use rustls_pki_types::{CertificateDer, PrivateKeyDer};
 use std::fs::File;
 use std::io::BufReader;
 use std::ops::Deref;
-use std::pin::Pin;
 use std::sync::Arc;
-use tokio::net::TcpListener;
+use tokio::net::{TcpListener, TcpStream};
 use tokio_rustls::TlsAcceptor;
 
 #[derive(Clone, Debug)]
@@ -82,7 +77,8 @@ impl MtlServer {
         server_key_path: Box<str>,
         client_ca_cert_path: Box<str>,
     ) -> Self {
-        let protocols = Some(vec![Protocol::HTTP_1, Protocol::HTTP_2].into_boxed_slice());
+        let protocols =
+            Some(vec![Protocol::HTTP_1, Protocol::HTTP_2].into_boxed_slice());
         Self {
             server_cert_path,
             server_key_path,
@@ -106,58 +102,65 @@ impl MtlServer {
         }
     }
 
-    fn load_cert(path: &str) -> Result<Vec<Certificate>, Error> {
+    fn load_cert(path: &str) -> Result<Vec<CertificateDer<'static>>, Error> {
         let cert_file = File::open(path).map_err(|x| {
             let msg = format!("failed to read certificate form path: {}", path);
             CertFileReadError(CertErrorDetail::new(msg, x))
         })?;
         let mut reader = BufReader::new(cert_file);
-        let certs = rustls_pemfile::certs(&mut reader).map_err(|x| {
-            let msg = format!("failed to extract certificate from file: {}", path);
-            CertExtractError(CertErrorDetail::new(msg, x))
-        })?;
-        Ok(certs.into_iter().map(Certificate).collect())
+        let certs: std::io::Result<Vec<CertificateDer>> =
+            rustls_pemfile::certs(&mut reader).collect();
+
+        let certs = match certs {
+            Ok(certs) => certs,
+            Err(err) => {
+                // todo: better error handling, the new error type and better message
+                return Err(CertFileReadError(CertErrorDetail::new(
+                    "Error reading certificate".into(),
+                    err,
+                )));
+            }
+        };
+        Ok(certs)
     }
 
-    fn load_server_cert(&self) -> Result<Vec<Certificate>, Error> {
+    fn load_server_cert(&self) -> Result<Vec<CertificateDer<'static>>, Error> {
         Self::load_cert(&self.server_cert_path)
     }
 
-    fn load_client_ca_cert(&self) -> Result<Vec<Certificate>, Error> {
+    fn load_client_ca_cert(
+        &self,
+    ) -> Result<Vec<CertificateDer<'static>>, Error> {
         Self::load_cert(&self.client_ca_cert_path)
     }
 
-    fn load_server_key(&self) -> Result<PrivateKey, Error> {
-        let key_file = File::open(self.server_key_path.deref()).map_err(PrivateKeyFileReadError)?;
+    fn load_server_key(&self) -> Result<PrivateKeyDer<'static>, Error> {
+        let key_file = File::open(self.server_key_path.deref())
+            .map_err(PrivateKeyFileReadError)?;
         let mut reader = BufReader::new(key_file);
 
-        let item = rustls_pemfile::read_one(&mut reader)
+        let item = rustls_pemfile::private_key(&mut reader)
             .map_err(PrivateKeyExtractError)?
             .ok_or_else(|| PrivateKeyItemEmptyError)?;
 
-        let key = match item {
-            Item::RSAKey(data) => PrivateKey(data),
-            Item::PKCS8Key(data) => PrivateKey(data),
-            Item::ECKey(data) => PrivateKey(data),
-            _ => return Err(PrivateKeyUnsupportedFormat),
-        };
-        Ok(key)
+        Ok(item)
     }
 
     fn create_tls_config(&self) -> Result<ServerConfig, Error> {
         let mut roots = RootCertStore::empty();
 
         let client_ca_certs = self.load_client_ca_cert()?;
-        for cert in &client_ca_certs {
+        for cert in client_ca_certs {
             roots.add(cert).map_err(TrustStoreError)?;
         }
 
-        let client_verifier = AllowAnyAuthenticatedClient::new(roots).boxed();
+        // todo: better error handling
+        let client_verifier =
+            WebPkiClientVerifier::builder(roots.into()).build().unwrap();
         let server_cert = self.load_server_cert()?;
         let server_key = self.load_server_key()?;
 
         let mut config = ServerConfig::builder()
-            .with_safe_defaults()
             .with_client_cert_verifier(client_verifier)
             .with_single_cert(server_cert, server_key)
             .map_err(ServerConfigError)?;
@@ -171,22 +174,22 @@ impl MtlServer {
         Ok(config)
     }
 
-    pub async fn serve<F>(&self, listener: TcpListener, callback: F) -> Result<(), Error>
+    pub async fn serve<F>(
+        &self,
+        listener: TcpListener,
+        callback: F,
+    ) -> Result<(), Error>
     where
-        F: Fn(AddrStream, TlsAcceptor) + 'static,
+        F: Fn(TcpStream, TlsAcceptor) + 'static,
     {
         let config = self.create_tls_config()?;
         let acceptor = TlsAcceptor::from(Arc::new(config));
-        let mut listener = AddrIncoming::from_listener(listener).map_err(ServeListenerError)?;
 
         loop {
-            let stream = poll_fn(|ctx| Pin::new(&mut listener).poll_accept(ctx)).await;
-
+            // todo: better error handling
+            let (stream, _remote_addr) = listener.accept().await.unwrap();
             let acceptor = acceptor.clone();
-
-            if let Some(Ok(stream)) = stream {
-                callback(stream, acceptor);
-            }
+            callback(stream, acceptor);
         }
     }
 }
